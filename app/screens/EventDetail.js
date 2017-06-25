886a1424b846ce1cb38432684fe2cbeb53471f03
alexdriedger <aldriedger@gmail.com>
2017-06-25 13:56:33 -0700

    Added image to event detail

--- app/screens/EventDetail.js
+++ app/screens/EventDetail.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  StyleSheet,
+  Image,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    height: 200,
+  },
+  titleContainer: {
+
+  },
+});
 
 class EventDetail extends Component {
   render() {
     const { item } = this.props.navigation.state.params;
     return (
-      <View>
-        <Text>{item.name}</Text>
-        <Text>{item.id}</Text>
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{uri: item.cover.source}}
+        />
+        <View style={styles.titleContainer}>
+          <Text>{item.name}</Text>
+          <Text>{item.owner.name}</Text>
+        </View>
       </View>
     );
   }
