1da02da88d8b0e09af2e05a00c7a6559188c2363
alexdriedger <aldriedger@gmail.com>
2017-08-22 22:40:21 -0700

    A basic fetch request that works

--- app/index.js
+++ app/index.js
@@ -4,6 +4,21 @@ import { RootStack } from './nav/routers';
 
 class App extends Component {
   render() {
+    // TODO : USE BASE 64 INSTEAD OF BTOA
+    // TODO : FIGURE OUT WHY FETCH IS UNDERLINED
+    fetch('https://fff-api-heroku.herokuapp.com/food/api/v1.0/schools/1/events', {
+      headers: {
+        Authorization: 'Basic ' + btoa('free_food_app' + ':' + 'FreeFoodIsTheBest'),
+      },
+    })
+    .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        return responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     return (
       <RootStack />
     );
