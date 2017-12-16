21589413efedf1401422d7aa2f7ffd38f362e6fc
Steph Martens <stephmartens@Stephs-MacBook.hitronhub.home>
2017-12-15 19:09:43 -0600

    Renmaed project

--- android/app/src/main/java/com/free/food/scavenge/MainActivity.java
+++ android/app/src/main/java/com/free/food/scavenge/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactfood;
+package com.free.food.scavenge;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactFood";
+        return "Scavenge";
     }
 }
--- ios/Pods/Target Support Files/Pods-ReactFoodTests/Pods-ReactFoodTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactFoodTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactFoodTests
-@end
